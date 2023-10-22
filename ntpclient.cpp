b50ccdde7d942b811a2b5411e01d126e6f9368a7
Edison Henrique Andreassy <ehasis@hotmail.com>
2023-10-22 20:25:55 -0300

    Change the source code structure.

--- ntpclient.cpp
+++ ntpclient.cpp
@@ -14,11 +14,9 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-	int ret = EXIT_SUCCESS;
-
-	bool sync = false;
+	auto sync = false;
 	char server[50] = "pool.ntp.org";
-	int port = 123;
+	auto port = 123;
 
 	// Check for command line arguments
 	for (int i = 1; i < argc; i++) {
@@ -31,95 +29,98 @@ int main(int argc, char* argv[]) {
 	}
 
 	// Initiates use of the Winsock DLL
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != S_OK) {
+	WSADATA wsadata;
+	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != S_OK) {
 		cerr << "Winsock startup failed." << endl;
-		ret = EXIT_FAILURE;
+		return EXIT_FAILURE;
 	}
-	else {
-		// Translate from an ANSI host name to an address
-		addrinfo* info = nullptr;
-		addrinfo hints;
-		ZeroMemory(&hints, sizeof(hints));
-		hints.ai_family = AF_INET;
-		hints.ai_socktype = SOCK_DGRAM;
-		if (getaddrinfo(server, nullptr, &hints, &info) != S_OK) {
-			cerr << "Get address info failed." << endl;
-			ret = EXIT_FAILURE;
-		}
-		else {
-			sockaddr_in addr{};
-			addr.sin_family = AF_INET;
-			addr.sin_port = htons(port);
-			addr.sin_addr = reinterpret_cast<sockaddr_in*>(info->ai_addr)->sin_addr;
 
-			freeaddrinfo(info);
-
-			// Creates a UDP socket
-			auto sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-			if (sock == INVALID_SOCKET) {
-				cerr << "Socket creation failed." << endl;
-				ret = EXIT_FAILURE;
-			}
-			else {
-				// Set the socket timeout
-				constexpr auto timeout = 5000;
-				constexpr auto optLen = sizeof(timeout);
-				auto optVal = reinterpret_cast<const char*>(&timeout);
-				setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, optVal, optLen);
-				setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, optVal, optLen);
-
-				constexpr auto PACKET_SIZE = 48;
-
-				// Send the NTP packet with the flags li=0, vn=4, mode=3
-				const char request[PACKET_SIZE] = { 0x23 };
-				if (sendto(sock, request, PACKET_SIZE, 0, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) == SOCKET_ERROR) {
-					cerr << "Socket send failed." << endl;
-					ret = EXIT_FAILURE;
-				}
-				else {
-					// Receive the NTP packet
-					char response[PACKET_SIZE] = { 0 };
-					if (recvfrom(sock, response, PACKET_SIZE, 0, nullptr, nullptr) == SOCKET_ERROR) {
-						cerr << "Socket receive failed." << endl;
-						ret = EXIT_FAILURE;
-					}
-					else {
-						// Convert the transmited time-stamp to little-endian
-						auto timestamp = ntohl(*reinterpret_cast<unsigned int*>(&response[40]));
-
-						// The number of seconds we get back from the server is equal to the no. of
-						// seconds since Jan 1, 1900. Since Unix time starts from Jan 1, 1970, we
-						// subtract 70 years worth of seconds from Jan 1, 1990.
-						// (1900)-------(1970)************************(Time Packet Left the Server)
-						auto ntpTime = static_cast<time_t>(timestamp - 2208988800u);
-
-						// Convert to tm structure
-						tm t;
-						localtime_s(&t, &ntpTime);
-
-						if (sync) {
-							// Update the computer time
-							SYSTEMTIME st{};
-							st.wYear = t.tm_year + 1900;
-							st.wMonth = t.tm_mon + 1;
-							st.wDay = t.tm_mday;
-							st.wHour = t.tm_hour;
-							st.wMinute = t.tm_min;
-							st.wSecond = t.tm_sec;
-							if (!SetLocalTime(&st)) {
-								cerr << "Set system time failed." << endl;
-								ret = EXIT_FAILURE;
-							}
-						}
-
-						cout << "NTP time: " << put_time(&t, "%Y-%m-%d %H:%M:%S") << endl;
-					}
-				}
-				closesocket(sock);
-			}
-		}
+	// Translate from an ANSI host name to an address
+	addrinfo* info = nullptr;
+	addrinfo hints;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	if (getaddrinfo(server, nullptr, &hints, &info) != S_OK) {
+		cerr << "Get address info failed." << endl;
 		WSACleanup();
+		return EXIT_FAILURE;
 	}
-	return ret;
+
+	sockaddr_in addr{};
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr = reinterpret_cast<sockaddr_in*>(info->ai_addr)->sin_addr;
+
+	freeaddrinfo(info);
+
+	// Creates a UDP socket
+	auto sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock == INVALID_SOCKET) {
+		cerr << "Socket creation failed." << endl;
+		WSACleanup();
+		return EXIT_FAILURE;
+	}
+
+	// Set the socket timeout to 5 seconds
+	constexpr auto TIME_OUT = 5000;
+	auto timeout = reinterpret_cast<const char*>(&TIME_OUT);
+	setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, timeout, sizeof(TIME_OUT));
+	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, timeout, sizeof(TIME_OUT));
+
+	constexpr auto PACKET_SIZE = 48;
+
+	// Send the NTP packet with the flags li=0, vn=4, mode=3
+	const char request[PACKET_SIZE] = { 0x23 };
+	if (sendto(sock, request, PACKET_SIZE, 0, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) == SOCKET_ERROR) {
+		cerr << "Socket send failed." << endl;
+		closesocket(sock);
+		WSACleanup();
+		return EXIT_FAILURE;
+	}
+
+	// Receive the NTP packet
+	char response[PACKET_SIZE] = { 0 };
+	if (recvfrom(sock, response, PACKET_SIZE, 0, nullptr, nullptr) == SOCKET_ERROR) {
+		cerr << "Socket receive failed." << endl;
+		closesocket(sock);
+		WSACleanup();
+		return EXIT_FAILURE;
+	}
+
+	closesocket(sock);
+
+	// Convert the transmited time-stamp to little-endian
+	auto timestamp = ntohl(*reinterpret_cast<unsigned int*>(&response[40]));
+
+	// The number of seconds we get back from the server is equal to the no. of
+	// seconds since Jan 1, 1900. Since Unix time starts from Jan 1, 1970, we
+	// subtract 70 years worth of seconds from Jan 1, 1990.
+	// (1900)-------(1970)************************(Time Packet Left the Server)
+	auto ntptime = static_cast<time_t>(timestamp - 2208988800u);
+
+	// Convert to tm structure
+	tm t;
+	localtime_s(&t, &ntptime);
+
+	if (sync) {
+		// Update the computer time
+		SYSTEMTIME st{};
+		st.wYear = t.tm_year + 1900;
+		st.wMonth = t.tm_mon + 1;
+		st.wDay = t.tm_mday;
+		st.wHour = t.tm_hour;
+		st.wMinute = t.tm_min;
+		st.wSecond = t.tm_sec;
+		if (!SetLocalTime(&st)) {
+			cerr << "Set system time failed." << endl;
+			WSACleanup();
+			return EXIT_FAILURE;
+		}
+	}
+
+	cout << "NTP time: " << put_time(&t, "%Y-%m-%d %H:%M:%S") << endl;
+
+	WSACleanup();
+	return EXIT_SUCCESS;
 }
